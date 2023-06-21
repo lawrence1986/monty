ab9b47eaea70649ccf7a12a2222c95ddaec3e69b
codeabuu <abdulngui718@gmail.com>
2023-06-21 05:31:27 -0700

    opcode pop

--- pop.c
+++ pop.c
@@ -1,23 +1,21 @@
 #include "monty.h"
+
 /**
- * f_pop - prints the top
- * @head: stack head
- * @count: line_number
- * Return: no return
+* f_pop - prints the top
+* @head: stack head
+* @count: line_number
+* Return: no return
 */
 void f_pop(stack_t **head, unsigned int count)
 {
-	stack_t *h;
+stack_t *h;
 
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", count);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	*head = h->next;
-	free(h);
+if (*head == NULL)
+{
+pop_exit(count, head);
+}
+
+h = *head;
+*head = h->next;
+free(h);
 }
