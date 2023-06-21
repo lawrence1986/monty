08f35e5cb205496268f22a499308a79cbbff90c6
codeabuu <abdulngui718@gmail.com>
2023-06-21 02:56:10 -0700

    updates

--- execute.c
+++ execute.c
@@ -2,7 +2,7 @@
 /**
 * execute - executes the opcode
 * @stack: head linked list - stack
-* @counter: line_counter
+* @count: line_counter
 * @file: poiner to monty file
 * @content: line content
 * Return: no return
