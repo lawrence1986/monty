1b0bbed278c5cb1ed4afda8e29a3db4176f7bf8b
codeabuu <abdulngui718@gmail.com>
2023-06-21 04:53:13 -0700

    push,pall

--- push.c
+++ push.c
@@ -1,41 +1,47 @@
 #include "monty.h"
+
 /**
  * f_push - add node to the stack
  * @head: stack head
- * @count: line_number
+ * @counter: line_number
  * Return: no return
 */
 void f_push(stack_t **head, unsigned int count)
 {
 	int n, j = 0, flag = 0;
+	char *error_message = "L%d: usage: push integer\n";
 
 	if (bus.arg)
 	{
 		if (bus.arg[0] == '-')
 			j++;
-		while (bus.arg[j] != '\0')
+		for (; bus.arg[j] != '\0'; j++)
 		{
 			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-			{
 				flag = 1;
-			}
-			j++;
 		}
 		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", count);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
+		{
+			goto exit_failure;
+		}
+	}
 	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", count);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
+	{
+		goto exit_failure;
+	}
+
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
 		addnode(head, n);
 	else
 		addqueue(head, n);
+
+	return;
+
+exit_failure:
+	fprintf(stderr, error_message, count);
+	fclose(bus.file);
+	free(bus.content);
+	free_stack(*head);
+	exit(EXIT_FAILURE);
 }
